9f18d97642294b52b39e78736acf033da2e4de89
wes <xd88021as@gmail.com>
2026-02-23 11:38:48 +0800

    feat: update role routes and swagger

--- src/modules/role/role.routes.ts
+++ src/modules/role/role.routes.ts
@@ -3,24 +3,37 @@ import { plainToInstance } from 'class-transformer';
 import { RoleController } from './controllers/role.controller';
 import { RoleCreateDTO, RoleUpdateDTO } from './dtos/role-request.dto';
 import { validateDTO } from '@middlewares/validate.middleware';
+import { authMiddleware, requireOwnershipOrRole } from '@middlewares/auth.middleware';
 
 const router = Router();
 
-router.post('', validateDTO(RoleCreateDTO), async (req, res) => {
-  const dto = plainToInstance(RoleCreateDTO, req.body);
-  const response = await RoleController.create(dto.name);
-  res.status(200).json(response);
-});
+router.post(
+  '',
+  authMiddleware,
+  requireOwnershipOrRole({ roles: ['admin'] }),
+  validateDTO(RoleCreateDTO),
+  async (req, res) => {
+    const dto = plainToInstance(RoleCreateDTO, req.body);
+    const response = await RoleController.create(dto.name);
+    res.status(200).json(response);
+  }
+);
 
 router.get('', async (req, res) => {
   const response = await RoleController.findMany();
   res.status(200).json(response);
 });
 
-router.patch('', validateDTO(RoleUpdateDTO), async (req, res) => {
-  const dto = plainToInstance(RoleUpdateDTO, req.body);
-  const response = await RoleController.update(dto.targetName, dto.newName);
-  res.status(200).json(response);
-});
+router.patch(
+  '',
+  authMiddleware,
+  requireOwnershipOrRole({ roles: ['admin'] }),
+  validateDTO(RoleUpdateDTO),
+  async (req, res) => {
+    const dto = plainToInstance(RoleUpdateDTO, req.body);
+    const response = await RoleController.update(dto.targetName, dto.newName);
+    res.status(200).json(response);
+  }
+);
 
 export default router;
