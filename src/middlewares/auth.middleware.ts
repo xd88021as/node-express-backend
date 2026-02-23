8204ae3ba2ceebd58f63f0bd6dafc7d204d7f068
wes <xd88021as@gmail.com>
2026-02-23 11:38:23 +0800

    feat: add requireOwnershipOrRole to auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import { TokenPayload, verifyJwt } from '@utils/jwt.util';
-import { checkNotFound, checkUnauthorized } from '@utils/http-error.util';
+import { checkForbidden, checkNotFound, checkUnauthorized } from '@utils/http-error.util';
 import { UserService } from '@modules/user/services/user.service';
+
+type RequestWithUser = Request & { user?: TokenPayload };
 
 export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
@@ -10,10 +12,20 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
     checkUnauthorized(payload, 'Invalid token');
     const user = await UserService.findUnique({ uuid: payload.userUuid });
     checkNotFound(user, 'User not found');
-    type RequestWithUser = Request & { user?: TokenPayload };
     (req as RequestWithUser).user = payload;
     next();
   } catch (error) {
     next(error);
   }
+}
+
+export function requireOwnershipOrRole(options: { roles?: string[]; allowSelf?: boolean }) {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const user = (req as RequestWithUser).user;
+    checkUnauthorized(user);
+    const hasRole = options.roles && options.roles.includes(user.roleName);
+    const isSelf = options.allowSelf && user.userUuid === req.params.uuid;
+    checkForbidden(hasRole || isSelf);
+    next();
+  };
 }
